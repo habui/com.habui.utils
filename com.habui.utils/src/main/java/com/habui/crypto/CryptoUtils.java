97c79297a261651a269e8bb1d2529453238da5e4
habui <buinguyensonha@yahoo.com>
2014-02-15 22:28:54 +0700

    update CryptoUtils

--- com.habui.utils/src/main/java/com/habui/crypto/CryptoUtils.java
+++ com.habui.utils/src/main/java/com/habui/crypto/CryptoUtils.java
@@ -4,8 +4,20 @@
  */
 package com.habui.crypto;
 
+import com.habui.log.LogUtils;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Security;
+import java.util.logging.Level;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.log4j.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
@@ -13,29 +25,95 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
  * @author habui
  */
 public class CryptoUtils {
-    
+    private static Logger logger = Logger.getLogger(CryptoUtils.class);
     private static BouncyCastleProvider bc = new BouncyCastleProvider();
     
     private Cipher encryptCipher = null;
-    private Cipher descryptCipher = null;
+    private Cipher decryptCipher = null;
+    
+    private EAlgorithm algorithm;
+    private EMode mode;
+    private EPaddingMode paddingMode;
     
     
     static {
         Security.addProvider(bc);
     }
     
-    public CryptoUtils(EAlgorithm algorithm, EMode mode, EPaddingMode paddingMode) throws Exception {
-        String method = String.format("%s/%s/%s", EAlgorithm.getAlgorithmName(algorithm), EMode.getModeName(mode), EPaddingMode.getPaddingMode(paddingMode));
-        encryptCipher = Cipher.getInstance(method, bc);
-        descryptCipher = Cipher.getInstance(method, bc);
+    public CryptoUtils(EAlgorithm algorithm, EMode mode, EPaddingMode paddingMode) {
+        try {
+            String method = String.format("%s/%s/%s", EAlgorithm.getAlgorithmName(algorithm), EMode.getModeName(mode), EPaddingMode.getPaddingMode(paddingMode));
+            encryptCipher = Cipher.getInstance(method, bc);
+            decryptCipher = Cipher.getInstance(method, bc);
+        
+            this.algorithm = algorithm; this.mode = mode; this.paddingMode = paddingMode;
+        }
+        catch (NoSuchAlgorithmException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (NoSuchPaddingException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        }
     }
     
-    public void encrypt(byte[] input, byte[] output) {
+    public byte[] encrypt(byte[] plainText, byte[] keyBytes, byte[] ivBytes)  {
+        if (ivBytes == null || ivBytes.length == 0)
+            return null;
         
+        IvParameterSpec ivSpec =new IvParameterSpec(ivBytes);
+        SecretKeySpec key = new SecretKeySpec(keyBytes, EAlgorithm.getAlgorithmName(this.algorithm));
+        
+        byte[] cipherText = null;
+        try {
+            encryptCipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+            
+            cipherText = new byte[encryptCipher.getOutputSize(plainText.length)];
+            int ctLength = encryptCipher.update(plainText, 0, plainText.length, cipherText, 0);
+            ctLength += encryptCipher.doFinal(cipherText, ctLength);
+            
+        }
+        catch (InvalidKeyException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (InvalidAlgorithmParameterException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (ShortBufferException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (IllegalBlockSizeException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (BadPaddingException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        }
+        return cipherText;
     }
     
-    public void descrypt(byte[] input, byte[] output) {
+    public byte[] descrypt(byte[] cipherText, byte[] keyBytes, byte[] ivBytes) {
+        if (ivBytes == null || ivBytes.length == 0)
+            return null;
         
+        IvParameterSpec ivSpec =new IvParameterSpec(ivBytes);
+        SecretKeySpec key = new SecretKeySpec(keyBytes, EAlgorithm.getAlgorithmName(this.algorithm));
+        
+        byte[] plainText = null;
+        try {
+            decryptCipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+            
+            plainText = new byte[decryptCipher.getOutputSize(cipherText.length)];
+            
+            int ptLength = decryptCipher.update(cipherText, 0, cipherText.length, plainText, 0);
+            ptLength += decryptCipher.doFinal(plainText, ptLength);
+            
+        }
+        catch (InvalidKeyException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (InvalidAlgorithmParameterException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (ShortBufferException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (IllegalBlockSizeException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        } catch (BadPaddingException ex) {
+            logger.error(LogUtils.stackTrace(ex));
+        }
+        return plainText;
     }
     
 }
