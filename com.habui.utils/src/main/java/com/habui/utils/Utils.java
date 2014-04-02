091be799aa582d7fef2ab1d48663bb1bfd7bb32a
Ha Bui <buinguyensonha@yahoo.com>
2014-04-03 05:18:57 +0700

    add Base64

--- com.habui.utils/src/main/java/com/habui/utils/Utils.java
+++ com.habui.utils/src/main/java/com/habui/utils/Utils.java
@@ -4,6 +4,10 @@
  */
 package com.habui.utils;
 
+import org.apache.commons.codec.binary.Base64;
+
+
+
 /**
  *
  * @author habns
@@ -11,5 +15,13 @@ package com.habui.utils;
 public class Utils {
     public static int getRandom(int lowerBound, int upperBound) {
         return lowerBound + (int)(Math.random() * ((upperBound - lowerBound) + 1));
+    }
+    
+    public static byte[] encodeBase64(String text) {
+        return Base64.encodeBase64(text.getBytes());
+    }
+    
+    public static byte[] decodeBase64(byte[] bytes) {
+        return Base64.decodeBase64(bytes);
     }
 }
